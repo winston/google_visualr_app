3ff6f5da250e78474ec8f6d0a289ecd83787e604
Winston Teo <winston.yongwei@gmail.com>
2010-05-19 11:01:42 +0800

    Added some meta goodness.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,18 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def meta_title
+
+    case
+      when controller_name.casecmp("Docs")    == 0
+        cname = "#{action_name.titleize} :: Documentation :: "
+      when controller_name.casecmp("Example") == 0
+        cname = "#{action_name.titleize} :: Examples :: "
+      else
+        cname = ""
+    end
+
+    return "#{cname}GoogleVisualr".gsub("Index ::", "")
+
+  end
+
 end
