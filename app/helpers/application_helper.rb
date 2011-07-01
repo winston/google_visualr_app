ac2ef068661576666d1b8ffff853c561bb81a32e
Winston <winston.yongwei@gmail.com>
2011-07-01 18:11:32 +0800

    Removed unused files. Fix meta title.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,15 @@
 module ApplicationHelper
 
   def meta_title
-
     case
-      when controller_name.casecmp("Docs")    == 0
-        cname = "#{action_name.titleize} :: Documentation :: "
-      when controller_name.casecmp("Example") == 0
-        cname = "#{action_name.titleize} :: Examples :: "
+      when controller_name.casecmp("docs") == 0
+        cname = "#{action_name.titleize} :: DataTable API Reference :: "
+      when controller_name.casecmp("interactive") == 0 || controller_name.casecmp("image") == 0
+        cname = "#{action_name.titleize} :: Chart Gallery :: "
       else
         cname = ""
     end
-
-    return "#{cname}GoogleVisualr".gsub("Index ::", "")
-
+    "#{cname}GoogleVisualr".gsub("Index ::", "")
   end
 
 end
