eee6a3a5df80c915a9c95cd61d8369aff86b6b54
Winston <winston.yongwei@gmail.com>
2011-07-04 23:05:24 +0800

    Add candlestick chart to gallery.

--- app/controllers/examples/interactive_controller.rb
+++ app/controllers/examples/interactive_controller.rb
@@ -72,6 +72,28 @@ class Examples::InteractiveController < ApplicationController
 
   end
 
+  # http://code.google.com/apis/chart/interactive/docs/gallery/candlestickchart.html
+  def candlestick_chart
+
+    data_table = GoogleVisualr::DataTable.new
+    data_table.new_column('string', 'day')
+    data_table.new_column('number', 'min')
+    data_table.new_column('number', 'opening')
+    data_table.new_column('number', 'closing')
+    data_table.new_column('number', 'max')
+    data_table.add_rows( [
+      ['Mon',20,28,38,45],
+      ['Tue',31,38,55,66],
+      ['Wed',50,55,77,80],
+      ['Thu',50,77,66,77],
+      ['Fri',15,66,22,68]
+    ] )
+
+    opts   = { :width => 400, :height => 240, :legend => 'none' }
+    @chart = GoogleVisualr::Interactive::CandlestickChart.new(data_table, opts)
+
+  end
+
   # http://code.google.com/apis/chart/interactive/docs/gallery/columnchart.html#Example
   def column_chart
 
