8f79aaab695b7e5349feef62560003a8efd55a2b
Winston <winston.yongwei@gmail.com>
2012-10-16 17:40:27 +0800

    Migrate to Rails 3.2.x

--- Gemfile
+++ Gemfile
@@ -1,8 +1,13 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.9'
+gem 'rails', '3.2.8'
 
-gem 'google_visualr', '~> 2.1.3'
+gem 'google_visualr', '~> 2.1.4'
+
+group :assets do
+  gem 'compass-rails' , '~> 1.0.3'
+  gem 'sass-rails'    , '~> 3.2.5'
+end
 
 group :development do
   gem 'heroku'
--- config/application.rb
+++ config/application.rb
@@ -5,9 +5,13 @@ require "active_resource/railtie"
 require "action_mailer/railtie"
 require "rails/test_unit/railtie"
 
-# If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require *Rails.groups(:assets => %w(development test))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
 
 module GoogleVisualrApp
   class Application < Rails::Application
@@ -41,5 +45,11 @@ module GoogleVisualrApp
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
   end
 end
